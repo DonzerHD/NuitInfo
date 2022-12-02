cce27fcabc27100a79b20463dfbdd0449a312206
DonzerHD <thomas.lemay59@outlook.fr>
2022-12-02 03:51:36 +0100

    biblio

--- js/consignes.js
+++ js/consignes.js
@@ -21,3 +21,39 @@ function afficheMessage(monTexte) {
 	nb_car = chaine.length;
 	id = setInterval("changeMessage()",20);
 }
+
+let i = 0;
+
+const virusSelect  = document.querySelector(".virusP1");
+virusSelect.addEventListener('click',()=>{
+     virusSelect.classList.remove("virus1");
+	 virusSelect.classList.add("virusD1");
+	 i++;
+	 if(i == 3){
+		afficheMessage("Cliquez sur la bibliothèque");
+	}
+}
+)
+
+const virusSelectS  = document.querySelector(".virusP2");
+virusSelectS.addEventListener('click',()=>{
+     virusSelectS.classList.remove("virus2");
+	 virusSelectS.classList.add("virusD2");
+	 i++;
+	 if(i == 3){
+		afficheMessage("Cliquez sur la bibliothèque");
+	}
+}
+)
+
+const virusSelectT  = document.querySelector(".virusP3");
+virusSelectT.addEventListener('click',()=>{
+     virusSelectT.classList.remove("virus3");
+	 virusSelectT.classList.add("virusD3");
+	 i++;
+	 if(i == 3){
+		afficheMessage("Cliquez sur la bibliothèque");
+	}
+}
+)
+
